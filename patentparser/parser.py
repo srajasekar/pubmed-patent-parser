3875186cce0573dc92700e64c07a1c77559856d7
ajbharani <none@none>
2012-03-10 01:15:25 -0500

    Cosmetics

--- patentparser/parser.py
+++ patentparser/parser.py
@@ -6,8 +6,8 @@ class ParserHelper:
 		self.rtext = ''
 	
 	def rectext(self, node, tag):
-		'''It is necessary that we have to probe into an abstract 
-		   recursively and pull out data wherever <p> tag appears'''
+		# It is necessary that we have to probe into an abstract 
+		# recursively and pull out data wherever <p> tag appears
 		for tagChild in node.childNodes:
 			if tagChild.nodeName == tag:
 				for pVals in tagChild.childNodes:
