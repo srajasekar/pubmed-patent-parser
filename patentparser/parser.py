dc4b98540201daf4a73190221754e6393a40fc79
ajbharani <none@none>
2012-03-10 03:12:56 -0500

    Added references fetch functionality

--- patentparser/parser.py
+++ patentparser/parser.py
@@ -53,10 +53,47 @@ class Parser:
 		# ['Ref1', 'Ref2']
 		# Ref:
 		# {'id':'id-val', 'type':'type-val', 'person-list':['list-of-persons'], 'source':'source-val', 
-		#	'article-title':'val', 'year':'val', 'pub-id-type':'val', 'pub-id':'val'}
+		#	'article-title':'val', 'year': 'val', 'pub-id-type':'val', 'pub-id':'val'}
 		# Person:
 		# {'type':'val', 'surname':'val', 'given-names':'val'}
-		pass
+		refs = []
+		rtags = self.dom.getElementsByTagName('back')
+		for rtag in rtags:
+			for reflist in rtag.childNodes:
+				if reflist.nodeName == 'ref-list':
+					for ref in reflist.childNodes:
+						if ref.nodeName == 'ref':
+							refdict = dict()
+							refdict['id'] = ref.getAttribute('id')
+							for refattr in ref.childNodes:
+								if refattr.nodeName == 'element-citation' or refattr.nodeName == 'mixed-citation':
+									refdict['type'] = refattr.getAttribute('publication-type')
+									for element in refattr.childNodes:
+										if element.nodeName == 'person-group':
+											grptype = element.getAttribute('person-group-type')
+											personDictList = []
+											for person in element.childNodes:												
+												if person.nodeName == 'name':
+													personDict = dict()
+													personDict['type'] = grptype
+													for name in person.childNodes:
+														if name.nodeName == 'surname':
+															personDict['surname'] = name.firstChild.data
+														elif name.nodeName == 'given-names':
+															personDict['given-names'] = name.firstChild.data
+													personDictList.append(personDict)
+											refdict['person-list'] = personDictList	
+										elif element.nodeName == 'source':
+											refdict['source'] = element.firstChild.data
+										elif element.nodeName == 'article-title':
+											refdict['article-title'] = element.firstChild.data
+										elif element.nodeName == 'year':
+											refdict['year'] = element.firstChild.data
+										elif element.nodeName == 'pub-id':
+											refdict['pub-id-type'] = element.getAttribute('pub-id-type')
+											refdict['pub-id'] = element.firstChild.data						
+							refs.append(refdict)
+		return refs
 	
 	def abstract(self):
 		# author : ajbharani
