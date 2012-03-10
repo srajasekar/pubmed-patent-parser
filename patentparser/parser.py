2785687934f66c8d1286546bb8034ff4f823e53b
ajbharani <none@none>
2012-03-10 01:19:52 -0500

    Adding helper file for abstract

--- patentparser/parser.py
+++ patentparser/parser.py
@@ -1,22 +1,5 @@
 from xml.dom.minidom import parse
-
-class ParserHelper:
-	
-	def __init__(self):
-		self.rtext = ''
-	
-	def rectext(self, node, tag):
-		# It is necessary that we have to probe into an abstract 
-		# recursively and pull out data wherever <p> tag appears
-		for tagChild in node.childNodes:
-			if tagChild.nodeName == tag:
-				for pVals in tagChild.childNodes:
-					if pVals.nodeType == 3:
-						self.rtext += pVals.data
-					else:
-						self.rectext(pVals, tag)
-			else:
-				self.rectext(tagChild, tag)
+from parserhelper import ParserHelper
 
 class Parser:
 	
