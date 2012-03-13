f4eab89c0a7a5ded3c89d3cb12348f48f364acd9
ajbharani <none@none>
2012-03-13 19:39:43 -0400

    Updated keywords table

--- pubmed_schema.sql
+++ pubmed_schema.sql
@@ -3,20 +3,12 @@
 -- pubmed_Article(Id, Abstract, Body)
 create table pubmed_Article(
     Id integer not null auto_increment,
-    ArticleId long varchar,
+    ArticleIds long varchar,
     ArticleKeywords long varchar,
     Abstract long varchar,
     Body long varchar,
     PubDate Date,
     constraint ARTPK1 primary key(Id)
-);
-
--- pubmed_Id(ArtilceId, PubIdType, PubId)
-create table pubmed_Id (
-    ArtilceId integer not null,
-    PubIdType varchar(32),
-    PubId varchar(32),
-    constraint IDFK1 foreign key(ArtilceId) references pubmed_Article(Id)
 );
 
 -- pubmed_Title(ArtilceId, Title)
@@ -36,9 +28,13 @@ create table pubmed_Contributor (
 );
 
 -- pubmed_Keyword(ArtilceId, Keyword)
-create table pubmed_Keyword (
+create table pubmed_NgramKeyword (
     ArtilceId integer not null,
-    Keyword varchar(256),
+    NgramKeyword long varchar,
+    NgramSize integer,
+    TF real,
+    IDF real,
+    PMI real,
     constraint KWFK1 foreign key(ArtilceId) references pubmed_Article(Id)
 );
 
