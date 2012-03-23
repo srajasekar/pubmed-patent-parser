9fd6a20f6dd5f3cfa9825fb46f3b46430f669555
ajbharani <none@none>
2012-03-23 19:20:26 -0400

    Added readme and keyword extraction library

---
+++ termextractor/extract.py
@@ -0,0 +1,23 @@
+from topia.termextract import extract
+import MySQLdb
+
+class TermExtractor(object):
+	
+	def __init__(self):
+		self.extractor = extract.TermExtractor()
+		self.extractor.filter = extract.DefaultFilter(singleStrengthMinOccur=3)
+	
+	def _removeNonAscii(self, string):
+		return filter(lambda x: ord(x) < 128, string)
+	
+	def _removePuncts(self, string):
+		punct_list = ['.', ',', '(', ')', '{', '}', '...']
+		for punct in punct_list:
+			if string.count(punct) > 0:
+				return False
+		else:
+			return True
+	
+	def extract(self, string):
+		string = self._removeNonAscii(string)
+		return filter(self._removePuncts, self.extractor(string))
